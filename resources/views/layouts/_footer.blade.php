54991e43e62c11eb7ce7277235cb0f967472feaf
yangfan <yf244190857@gmail.com>
2020-09-06 15:35:46 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <div class="container">
+    <img class="brand-icon" src="/images/icon.png" alt="">
+    <a href="#">刻意练习，每日精进</a>
+
+    <div class="float-right">
+      <a href="/about">关于</a>
+    </div>
+  </div>
+</footer>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,27 +9,10 @@
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-  <nav class="navbar navbar-expand-md navbar-light">
-    <a class="navbar-brand" href="/">Weibo app</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="/help">帮助</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/about">关于</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">登录</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+  @include('layouts._header')
   <div class="container">
     @yield('content')
   </div>
+  @include('layouts._footer')
 </body>
 </html>
