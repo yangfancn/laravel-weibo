01a868b54cd1e0afffa8a31d0403705a4a24ad12
yangfan <yf244190857@gmail.com>
2020-09-06 14:59:21 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,8 +6,30 @@
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title', 'Weibo app') - Laravel 入门教程</title>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-  @yield('content')
+  <nav class="navbar navbar-expand-md navbar-light" style="background-color: #e3f2fd;">
+    <a class="navbar-brand" href="/">Weibo app</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/help">帮助</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">关于</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">登录</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="container">
+    @yield('content')
+  </div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -3,5 +3,17 @@
 @section('title', 'Weibo 主页')
 
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      你现在看到的是
+      <a href="javascript:;">Laravel 入门教程的实例主页</a>
+    </p>
+    <p>
+      一切，将从这里开始
+    </p>
+    <p>
+      <a href="#" class="btn btn-success btn-lg">现在注册</a>
+    </p>
+  </div>
 @stop
