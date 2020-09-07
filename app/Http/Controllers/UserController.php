f5361b5380aca1880ea62794b95d89aa18e65af6
yangfan <yf244190857@gmail.com>
2020-09-07 20:49:12 +0800

    用户显示页面

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function create()
     {
-        return view('user.create');
+        return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
