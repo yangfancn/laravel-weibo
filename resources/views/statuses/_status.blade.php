6e20ab161ff176c8f92d5fea0d014222dcf96152
yangfan <yf244190857@gmail.com>
2020-09-11 21:53:09 +0800

    删除微博

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -6,4 +6,13 @@
     <h5 class="mt-0 mb-1">{{ $user->name }} <small>/ {{ $status->created_at->diffForHumans() }}</small></h5>
     {{ $status->content }}
   </div>
+  @can('destroy', $status)
+    <form action="{{ route('statuses.destroy', $status->id) }}"
+          method="POST" onsubmit="return confirm('您确定删除本条微博吗？')">
+      {{ csrf_field() }}
+      {{ method_field('DELETE') }}
+
+      <button class="btn btn-danger btn-sm" type="submit">删除</button>
+    </form>
+  @endcan
 </li>
